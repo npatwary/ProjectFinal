0df3114e49ecce474ffd1364f403a6a91a46871b
tbaniya <tbaniya@memphis.edu>
2015-04-04 17:22:06 +0000

    user model only

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  user_name  :string
+#  email      :string
+#  password   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+
+
+	validates :user_name, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true
+
+
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  user_name  :string
+#  email      :string
+#  password   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+	
+	def setup
+		@user = User.new(user_name: "tbaniya", email: "abc@memphis.edu", password:"xyslkd11k")
+	end
+
+	test "should be valid " do 
+ 		assert @user.valid?
+ 	end
+ 	test " user_name should be present" do
+ 		@user.user_name = "  "
+ 		assert_not @user.valid?
+ 	end
+ 	test "email should be present" do
+ 		@user.email = " "
+ 		assert_not @user.valid?
+ 	end
+ 	test "password should be present" do
+ 		@user.password = " "
+ 		assert_not @user.valid?
+ 	end
+
+end
