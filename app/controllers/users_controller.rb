06c2fb0ac00f323beadbd787fa2ad13fbe0c44f2
tbaniya <tbaniya@memphis.edu>
2015-04-05 14:13:15 +0000

    some

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+
+	def create
+		@user = User.new(params.require(:user).permit(:user_name, :email, :password))
+
+  		if @user.save
+  			redirect_to users_url
+  		else
+  			redirect_to new_user_url
+  		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  get 'users', to:'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
