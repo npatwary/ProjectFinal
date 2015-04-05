c65e263fcdee1ff0ff928b3b670329567ffc6d0e
nabinmaharjan <nmharjan@memphis.edu>
2015-04-05 23:40:56 +0000

    updated db/schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -11,44 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405181744) do
-
-  create_table "game_invitations", force: :cascade do |t|
-    t.string   "game_password"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.integer  "game_id"
-    t.integer  "dungeon_master_id"
-    t.integer  "user_player_id"
-  end
-
-  add_index "game_invitations", ["dungeon_master_id"], name: "index_game_invitations_on_dungeon_master_id"
-  add_index "game_invitations", ["game_id"], name: "index_game_invitations_on_game_id"
-  add_index "game_invitations", ["user_player_id"], name: "index_game_invitations_on_user_player_id"
-
-  create_table "games", force: :cascade do |t|
-    t.string   "name"
-    t.string   "map"
-    t.text     "game_history"
-    t.string   "game_password"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "user_id"
-  end
-
-  add_index "games", ["name"], name: "index_games_on_name", unique: true
-  add_index "games", ["user_id"], name: "index_games_on_user_id"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "user_name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
+ActiveRecord::Schema.define(version: 20150405183510) do
 
   create_table "ability_scores", force: :cascade do |t|
     t.integer  "PlayerCharacter_id"
@@ -93,6 +56,32 @@ ActiveRecord::Schema.define(version: 20150405181744) do
   end
 
   add_index "attack_weapons", ["PlayerCharacter_id"], name: "index_attack_weapons_on_PlayerCharacter_id"
+
+  create_table "game_invitations", force: :cascade do |t|
+    t.string   "game_password"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "game_id"
+    t.integer  "dungeon_master_id"
+    t.integer  "user_player_id"
+  end
+
+  add_index "game_invitations", ["dungeon_master_id"], name: "index_game_invitations_on_dungeon_master_id"
+  add_index "game_invitations", ["game_id"], name: "index_game_invitations_on_game_id"
+  add_index "game_invitations", ["user_player_id"], name: "index_game_invitations_on_user_player_id"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.string   "map"
+    t.text     "game_history"
+    t.string   "game_password"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+  end
+
+  add_index "games", ["name"], name: "index_games_on_name", unique: true
+  add_index "games", ["user_id"], name: "index_games_on_user_id"
 
   create_table "player_characters", force: :cascade do |t|
     t.boolean  "isUsed"
@@ -164,6 +153,17 @@ ActiveRecord::Schema.define(version: 20150405181744) do
   end
 
   add_index "skills", ["PlayerCharacter_id"], name: "index_skills_on_PlayerCharacter_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
 
   create_table "wealths", force: :cascade do |t|
     t.integer  "PlayerCharacter_id"
