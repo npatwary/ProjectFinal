ed744015788ef2dedbd9d25ce3d3286eeb95df0b
RojobaYasmin <ryasmin@memphis.edu>
2015-04-12 21:52:36 +0000

    Resolved issues

--- test/models/invited_games_model_test.rb
+++ test/models/invited_games_model_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: invited_games_models
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  dungeonMaster :string
+#  passKey       :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class InvitedGamesModelTest < ActiveSupport::TestCase
