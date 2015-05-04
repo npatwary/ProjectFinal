e02e35490e4a9d5489ba243cd4fa4c096c2ef8b2
tbaniya <tbaniya@memphis.edu>
2015-05-04 00:33:24 +0000

    all test success after merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150502232306) do
-
 
   create_table "ability_scores", force: :cascade do |t|
     t.string   "name"
